28bde0b0dc432135eab7c4688a753e90b9e007de
sseekkaarr <sekarjenar.sj@gmail.com>
2024-12-15 22:59:47 +0700

    update content library page

--- pages/content-library.tsx
+++ pages/content-library.tsx
@@ -1,11 +1,15 @@
 import Header from '../components/Header';
+import styles from "../styles/ContentLibrary.module.scss";
 
 const ContentLibrary = () => {
   return (
     <>
       <Header />
-      <main>
-        <h1>Content Library</h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}> Content Library</h1>
+        <p className={styles.description}>
+          Oops! Looks like this feature is still brewing. Please check back later!
+        </p>
       </main>
     </>
   );
